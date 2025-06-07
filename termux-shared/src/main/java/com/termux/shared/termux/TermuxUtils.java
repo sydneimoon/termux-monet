1261326073fe59a54310d6be38b51cf3d0dc4e55
sydneimoon <213307359+sydneimoon@users.noreply.github.com>
2025-06-06 20:28:48 -0400

    Update TermuxUtils.java

--- termux-shared/src/main/java/com/termux/shared/termux/TermuxUtils.java
+++ termux-shared/src/main/java/com/termux/shared/termux/TermuxUtils.java
@@ -516,9 +516,9 @@ public class TermuxUtils {
         if (context == null)
             return "null";
         StringBuilder markdownString = new StringBuilder();
-        markdownString.append("## Termux:Monet <span style='font-size:5px; float:right;'>2.0</span>\n");
+        markdownString.append("## Termux:Monet `2.0`");
         //markdownString.append("## Termux:Monet");
-        markdownString.append("\n\n").append(MarkdownUtils.getLinkMarkdownString("Source code", "https://github.com/Termux-Monet/termux-monet")).append(" ( by `simplythebest` ) ");
+        markdownString.append("\n\n").append(MarkdownUtils.getLinkMarkdownString("Source Code", "https://github.com/Termux-Monet/termux-monet")).append(" ( by `simplythebest` ) ");
         markdownString.append("\n\n").append(context.getString(R.string.msg_about_termux_monet));
         markdownString.append("\n\n## Important Links");
         markdownString.append("\n\n### GitHub\n");
