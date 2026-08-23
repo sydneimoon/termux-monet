4f99904ee6f4ca475dbeaf4fbeddd8141aa2fa64
agnostic-apollo <agnosticapollo@gmail.com>
2026-08-24 01:50:37 +0500

    Security(RunCommandService): Do not send result back to any file based result config before `allow-external-app` property has been checked to be `true`
    The `RUN_COMMAND` permission is still enforced before `allow-external-apps` property state is checked.
    
    After running following command, it requires Termux app to be restarted or the `termux-reload-settings` command to be run for the updated `allow-external-app=true` to be loaded from the properties file.
    
    ```
    am startservice --user 0 -n com.termux/com.termux.app.RunCommandService \
    -a com.termux.RUN_COMMAND \
    --es com.termux.RUN_COMMAND_PATH '/data/data/com.termux/files/usr/bin/bash' \
    --esa com.termux.RUN_COMMAND_ARGUMENTS '-c,echo success; sleep 3;' \
    --es com.termux.RUN_COMMAND_WORKDIR '/data/data/com.termux/files/home' \
    --ez com.termux.RUN_COMMAND_BACKGROUND 'false' \
    --ez com.termux.RUN_COMMAND_RESULT_SINGLE_FILE 'true' \
    --es com.termux.RUN_COMMAND_RESULT_DIRECTORY '/data/data/com.termux/files/home/.termux' \
    --es com.termux.RUN_COMMAND_RESULT_FILE_BASENAME 'termux.properties' \
    --es com.termux.RUN_COMMAND_RESULT_FILE_ERROR_FORMAT 'allow-external-apps=true'
    ```
    
    Credits to @waihankan for the security report.

--- app/src/main/java/com/termux/app/RunCommandService.java
+++ app/src/main/java/com/termux/app/RunCommandService.java
@@ -116,14 +116,7 @@ public class RunCommandService extends Service {
         executionCommand.commandHelp = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_COMMAND_HELP, null);
         executionCommand.isPluginExecutionCommand = true;
         executionCommand.resultConfig.resultPendingIntent = intent.getParcelableExtra(RUN_COMMAND_SERVICE.EXTRA_PENDING_INTENT);
-        executionCommand.resultConfig.resultDirectoryPath = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_DIRECTORY, null);
-        if (executionCommand.resultConfig.resultDirectoryPath != null) {
-            executionCommand.resultConfig.resultSingleFile = intent.getBooleanExtra(RUN_COMMAND_SERVICE.EXTRA_RESULT_SINGLE_FILE, false);
-            executionCommand.resultConfig.resultFileBasename = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILE_BASENAME, null);
-            executionCommand.resultConfig.resultFileOutputFormat = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILE_OUTPUT_FORMAT, null);
-            executionCommand.resultConfig.resultFileErrorFormat = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILE_ERROR_FORMAT, null);
-            executionCommand.resultConfig.resultFilesSuffix = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILES_SUFFIX, null);
-        }
+
         // If "allow-external-apps" property to not set to "true", then just return
         // We enable force notifications if "allow-external-apps" policy is violated so that the
         // user knows someone tried to run a command in termux context, since it may be malicious
@@ -135,6 +128,28 @@ public class RunCommandService extends Service {
             TermuxPluginUtils.processPluginExecutionCommandError(this, LOG_TAG, executionCommand, true);
             return stopService();
         }
+
+        // Do not send result back to any file based result config before "allow-external-app"
+        // property has been ensured to be "true", otherwise clients can overwrite files inside
+        // Termux home or prefix, or external storage (if Termux has been granted permission) using
+        // `ResultConfig.resultFileErrorFormat` as `ResultSender.sendCommandResultDataToDirectory()`
+        // uses client controlled format passed to `String.format()` that is used to set the error
+        // file content, which can even be used to overwrite `termux.properties` file to set
+        // `allow-external-app=true` in it or any other shell rc file. The client would still need
+        // to have the `RUN_COMMAND` permission before it could do this.
+        // Note that clients waiting for file based result will hang if "allow-external-app" property
+        // is not "true" if we do not send the result, but a notification will still be shown for
+        // clients to know. A result is still sent if pending intent is used as that does not have
+        // this security issue.
+        executionCommand.resultConfig.resultDirectoryPath = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_DIRECTORY, null);
+        if (executionCommand.resultConfig.resultDirectoryPath != null) {
+            executionCommand.resultConfig.resultSingleFile = intent.getBooleanExtra(RUN_COMMAND_SERVICE.EXTRA_RESULT_SINGLE_FILE, false);
+            executionCommand.resultConfig.resultFileBasename = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILE_BASENAME, null);
+            executionCommand.resultConfig.resultFileOutputFormat = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILE_OUTPUT_FORMAT, null);
+            executionCommand.resultConfig.resultFileErrorFormat = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILE_ERROR_FORMAT, null);
+            executionCommand.resultConfig.resultFilesSuffix = IntentUtils.getStringExtraIfSet(intent, RUN_COMMAND_SERVICE.EXTRA_RESULT_FILES_SUFFIX, null);
+        }
+
         // If executable is null or empty, then exit here instead of getting canonical path which would expand to "/"
         if (executionCommand.executable == null || executionCommand.executable.isEmpty()) {
             errmsg = this.getString(R.string.error_run_command_service_mandatory_extra_missing, RUN_COMMAND_SERVICE.EXTRA_COMMAND_PATH);
